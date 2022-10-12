340a882083cdc3a9e32ffe3c0305eba1d7341cd7
Ruchika Sharma <ruchikas@geekyants.com>
2022-10-12 14:51:14 +0530

    fix:Follow-css-style-changes

--- components/Follow.js
+++ components/Follow.js
@@ -1,9 +1,8 @@
 import Image from "next/image";
 const Follow = ({ follow }) => {
   return (
-    <div className="hover:cursor-pointer px-2 py-2 hover:bg-[#1D1F23] transition duration-200">
-      <div>
-        <div className=" flex items-center justify-around">
+    <div className="hover:cursor-pointer px-4 py-2 flex items-center justify-between hover:bg-[#1D1F23] transition duration-200">
+          <div className="flex items-center space-x-3">
           <Image
             className="rounded-full"
             width={50}
@@ -12,17 +11,17 @@ const Follow = ({ follow }) => {
             src={follow.img}
             alt=""
           ></Image>
-          <div>
+          <div >
+
             <h6 className="text-white text-md font-bold">{follow.name}</h6>
             <p className=" text-sm font-medium text-gray-500 ">
               @{follow.userName}
             </p>
           </div>
-          <button className="bg-white items-center text-black px-2 rounded-full outline-none inline-block font-bold shadow-md hover:brightness-95 text-lg">
+          </div>
+          <button className="bg-white items-center text-black px-4 py-2 rounded-full outline-none inline-block font-bold shadow-md hover:brightness-95 text-sm">
             Follow
           </button>
-        </div>
-      </div>
     </div>
   );
 };
