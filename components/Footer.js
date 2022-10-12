313a4590f781c1ea7e39339467ad3f8c74c39302
Ruchika Sharma <ruchikas@geekyants.com>
2022-10-12 14:27:55 +0530

    Footer component

---
+++ components/Footer.js
@@ -0,0 +1,61 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+          <nav className="flex flex-row flex-wrap justify-center items-center text-sm">
+            <a href="#" className="no-underline text-stone-600 capitalize">
+              About
+            </a>
+            <a href="#" className="no-underline text-stone-600 capitalize">
+              help center
+            </a>
+            <a href="#" className="no-underline text-stone-600 capitalize">
+              terms of service
+            </a>
+            <a href="#" className="no-underline text-stone-600 capitalize">
+              privacy policy
+            </a>
+            <a href="#" className="no-underline text-stone-600 capitalize">
+              cookie policy
+            </a>
+            <a href="#" className="no-underline text-stone-600 capitalize">
+              ads info
+            </a>
+            <a href="#" className="no-underline text-stone-600 capitalize">
+              blog
+            </a>
+            <a href="#" className="no-underline text-stone-600 capitalize">
+              status
+            </a>
+            <a href="#" className="no-underline text-stone-600 capitalize">
+              careers
+            </a>
+            <a href="#" className="no-underline text-stone-600 capitalize">
+              brand resources
+            </a>
+            <a href="#" className="no-underline text-stone-600 capitalize">
+              advertising
+            </a>
+            <a href="#" className="no-underline text-stone-600 capitalize">
+              marketing
+            </a>
+            <a href="#" className="no-underline text-stone-600 capitalize">
+              twitter for business
+            </a>
+            <a href="#" className="no-underline text-stone-600 capitalize">
+              developers
+            </a>
+            <a href="#" className="no-underline text-stone-600 capitalize">
+              directory
+            </a>
+            <a href="#" className="no-underline text-stone-600 capitalize ">
+              settings
+            </a>
+            <div className="no-underline text-stone-600 capitalize text-lg">
+              &copy; 2021 Twitter, Inc.
+            </div>
+          </nav>
+        </div>
+  )
+}
+
+export default Footer
--- components/Home.js
+++ components/Home.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Link from "next/link";
 // import { useState } from "react";
 // import {SigninModal} from '../pages/signin';
-
+import Footer from './Footer';
 const Home = () => {
   // const [showSignupModal, setShowSignupModal] = useState(false);
   // const [showSigninModal, setShowSigninModal] = useState(false);
@@ -62,61 +62,7 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="footer">
-          <nav className="flex flex-row flex-wrap justify-center items-center text-sm">
-            <a href="#" className="no-underline text-stone-600 capitalize">
-              About
-            </a>
-            <a href="#" className="no-underline text-stone-600 capitalize">
-              help center
-            </a>
-            <a href="#" className="no-underline text-stone-600 capitalize">
-              terms of service
-            </a>
-            <a href="#" className="no-underline text-stone-600 capitalize">
-              privacy policy
-            </a>
-            <a href="#" className="no-underline text-stone-600 capitalize">
-              cookie policy
-            </a>
-            <a href="#" className="no-underline text-stone-600 capitalize">
-              ads info
-            </a>
-            <a href="#" className="no-underline text-stone-600 capitalize">
-              blog
-            </a>
-            <a href="#" className="no-underline text-stone-600 capitalize">
-              status
-            </a>
-            <a href="#" className="no-underline text-stone-600 capitalize">
-              careers
-            </a>
-            <a href="#" className="no-underline text-stone-600 capitalize">
-              brand resources
-            </a>
-            <a href="#" className="no-underline text-stone-600 capitalize">
-              advertising
-            </a>
-            <a href="#" className="no-underline text-stone-600 capitalize">
-              marketing
-            </a>
-            <a href="#" className="no-underline text-stone-600 capitalize">
-              twitter for business
-            </a>
-            <a href="#" className="no-underline text-stone-600 capitalize">
-              developers
-            </a>
-            <a href="#" className="no-underline text-stone-600 capitalize">
-              directory
-            </a>
-            <a href="#" className="no-underline text-stone-600 capitalize ">
-              settings
-            </a>
-            <div className="no-underline text-stone-600 capitalize text-lg">
-              &copy; 2021 Twitter, Inc.
-            </div>
-          </nav>
-        </div>
+       <Footer/>
       </div>
       {/* <SignupModal
         open={showSignupModal}
